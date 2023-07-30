e83ed745cde2c2f54532c8e839c6dd3f65a19581
Mushfiq-alt <mushfiquzzaman.chowdhury@gmail.com>
2023-07-30 23:12:23 +0600

    Implement Right Side Component Login With Google and Github

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
 import Marquee from "react-fast-marquee";
@@ -15,12 +15,36 @@ const Header = () => {
             </div>
             <div className='bg-light p-3'>
                 <div className='d-flex'>
-                    <Button variant="danger" className='px-4 py-2 rounded'>Latest</Button>
+                    <Button variant="danger" className='px-4 py-2 rounded'>Breaking New !</Button>
                     <Marquee speed={50} style={{ fontWeight: 'bold' }}>
                         Match Highlights: Germany vs Spain - as it happened ! Match Highlights: Germany vs Spain - as it happened !
                     </Marquee>
                 </div>
             </div>
+            <Navbar collapseOnSelect expand="lg" className="">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto">
+                            <Nav.Link href="#features">Latest</Nav.Link>
+                            <Nav.Link href="#pricing">Politics</Nav.Link>
+                            <Nav.Link href="#pricing">Crime</Nav.Link>
+                            <Nav.Link href="#pricing">World</Nav.Link>
+                            <Nav.Link href="#pricing">Trade</Nav.Link>
+                            <Nav.Link href="#pricing">Opinion</Nav.Link>
+                            <Nav.Link href="#pricing">Entertainment</Nav.Link>
+                            <Nav.Link href="#pricing">Lifestyle</Nav.Link>
+                            
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Profile</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                <Button variant='secondary'>Login</Button>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </Container>
     );
 };
--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,8 +1,23 @@
+import { Button, ListGroup } from "react-bootstrap";
+import { FaGithub, FaGoogle, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+
 
 const RightNav = () => {
     return (
-        <div>
-            <h2>Right Nav</h2>
+        <div className="mt-5 mr-10">
+            <h4>Login With</h4>
+            <div className="mt-3">
+                <Button variant="outline-primary"> <FaGoogle /> Login With Google</Button>
+                <Button variant="outline-secondary" className="mt-2"> <FaGithub /> Login With Github</Button>
+            </div>
+            <div className="mt-5">
+                <h4>Find Us On</h4>
+                <ListGroup>
+                    <ListGroup.Item className="mt-2"><FaFacebook/>Facebook</ListGroup.Item>
+                    <ListGroup.Item className="mt-2"><FaTwitter/>Twitter</ListGroup.Item>
+                    <ListGroup.Item className="mt-2"><FaInstagram/>Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
         </div>
     );
 };
