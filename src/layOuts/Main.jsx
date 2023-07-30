1a8d550fec5d99513740629b2eea690b28604bdc
Mushfiq-alt <mushfiquzzaman.chowdhury@gmail.com>
2023-07-30 15:13:30 +0600

    Left Nav Right Nav and Main Content Structure is created

--- src/layOuts/Main.jsx
+++ src/layOuts/Main.jsx
@@ -1,10 +1,28 @@
+import Container from 'react-bootstrap/Container';
 import Footer from "../pages/Shared/Footer/Footer";
 import Header from "../pages/Shared/Header/Header";
+import { Col, Row } from "react-bootstrap";
+import LeftNav from '../pages/Shared/LeftNav/LeftNav';
+import RightNav from '../pages/Shared/RightNav/RightNav';
+
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
+            <Container>
+                <Row>
+                    <Col lg={3}>
+                        <LeftNav></LeftNav>
+                    </Col>
+                    <Col lg={6}>
+                        <h2>Main Content Coming...</h2>
+                    </Col>
+                    <Col lg={3}>
+                        <RightNav></RightNav>
+                    </Col>
+                </Row>
+            </Container>
             <Footer></Footer>
         </div>
     );
