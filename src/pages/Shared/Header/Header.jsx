90eb280f9e86ca5aa82763a37fee99853e3ac4f8
Mushfiq-alt <mushfiquzzaman.chowdhury@gmail.com>
2023-07-30 20:06:37 +0600

    installed Marquee and apply that inside header component for breaking news

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,16 +1,27 @@
+import { Button, Container } from 'react-bootstrap';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
+import Marquee from "react-fast-marquee";
+
 
 const Header = () => {
     return (
-        <div>
+        <Container>
             <div className='text-center'>
 
                 <img src={logo} alt="" />
                 <p className=''><small>Journalism Without Fear or Favour</small></p>
                 <p>{moment().format("dddd, MMM Do YYYY")}</p>
             </div>
-        </div>
+            <div className='bg-light p-3'>
+                <div className='d-flex'>
+                    <Button variant="danger" className='px-4 py-2 rounded'>Latest</Button>
+                    <Marquee speed={50} style={{ fontWeight: 'bold' }}>
+                        Match Highlights: Germany vs Spain - as it happened ! Match Highlights: Germany vs Spain - as it happened !
+                    </Marquee>
+                </div>
+            </div>
+        </Container>
     );
 };
 
